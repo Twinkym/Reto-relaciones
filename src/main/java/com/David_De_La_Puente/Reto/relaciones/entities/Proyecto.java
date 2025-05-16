428c2f9ae2a755f35fe9ddd3fe20da7ebecad89d
David De La Puente <Twinkymen@hotmail.com>
2025-05-16 14:09:29 +0200

    Update Proyecto.java
    Eliminado un atributo innecesario

--- src/main/java/com/David_De_La_Puente/Reto/relaciones/entities/Proyecto.java
+++ src/main/java/com/David_De_La_Puente/Reto/relaciones/entities/Proyecto.java
@@ -16,7 +16,6 @@ public class Proyecto {
     private String nombre;
     private String descripcion;
     private LocalDate fechaInicio;
-    private LocalDate fechaFin;
     private Boolean activo;
 
     public Proyecto() {
